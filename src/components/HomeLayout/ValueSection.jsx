0ab8657e5bda12a7574496dfa0e1019f500ad7b4
matmaxgraphics <gbadamosimateen@gmail.com>
2025-02-04 07:54:51 +0100

    added referral section

--- src/components/HomeLayout/ValueSection.jsx
+++ src/components/HomeLayout/ValueSection.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
 
 function ValueSection() {
   return (
-    <div>ValueSection</div>
-  )
+    <section className="value-section">
+      <div className="container max-width">
+        <div className="section-flex">
+          <article className="text-drscription">
+            <h2>The Unfair Advantage You’ve Been Searching For</h2>
+            <p>
+              VolumeVault doesn’t just level the playing field, it tips it in
+              your favor. While others struggle for visibility, liquidity, and
+              community growth, you’ll rise above the competition
+            </p>
+            <h4>
+              This isn’t just volume -{" "}
+              <span>it’s visibility, growth, and dominance.</span>
+            </h4>
+          </article>
+
+          <article className="img-container">
+            <div className="value--item">
+              <span>01</span>
+              <p>Unmatched volume boosts that command attention.</p>
+            </div>
+            <div className="value--item">
+              <span>02</span>
+              <p>
+                Enhanced trust and credibility as new holders flock to your
+                project.
+              </p>
+            </div>
+            <div className="value--item">
+              <span>03</span>
+              <p>
+                A buzz-worthy presence that traders and investors can’t ignore.
+              </p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default ValueSection
+export default ValueSection;
